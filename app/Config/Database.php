11ca22a0611e7bafc5fc99c0ea27adb823a94a00
Javier Sanchez <javsan77@gmail.com>
2022-06-29 21:35:14 -0500

    config database parameters

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,9 +33,9 @@ class Database extends Config
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => '',
-        'password' => '',
-        'database' => '',
+        'username' => 'admin',
+        'password' => 'admin',
+        'database' => 'phonebook',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,

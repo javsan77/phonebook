feeb591ff6ebe8f9ad27177a05ab1b4911136f8d
Javier Sanchez <javsan77@gmail.com>
2022-07-01 02:09:56 -0500

    Testing communication between views and controllers

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,12 +8,17 @@ class Home extends Controller
 {
 
     public function index(){
-
         $model = new Model_phone();
-
         $data['list'] = $model->Get_All();
-
         return view('home',$data);
     }
+
+    public function search(){
+        echo ($_POST['text']);
+
+        echo json_encode("abcdde");
+
+    }
+
 
 }
--- app/Views/home.php
+++ app/Views/home.php
@@ -2,6 +2,10 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <title>Untitled Document</title>
 </head>
@@ -16,6 +20,7 @@
 
 	<p><h1>Phone Book App</h1></p>
 	<p>&nbsp;</p>
+	<form id="#form1">
 	<table>
 		<tr>
 			<td><p><h2>Contacts</h2></p> </td>
@@ -23,8 +28,9 @@
 			<td><p><button type="button" class="btn btn-primary">+Add Contact</button></p></td>
 		</tr>
 		<tr>
-			<td colspan="3"><input class="form-control" type="text" placeholder="Search for contact by last name..."></td>
-			<td><button type="button" class="btn btn-secondary">Search</button></td>
+		  
+			<td colspan="3"><input id="search_contact" class="form-control" type="search" placeholder="Search for contact by last name..."></td>
+			<td><button id="search" type="button" class="btn btn-secondary">Search</button></td>
 		</tr>
 		<tr>
 			<td colspan="4">&nbsp;</td>
@@ -40,10 +46,38 @@
 		}
 		?>
 	</table>
+	</form>
 
 
 	<!--<p>Tableview (foreach nombre-apellido / numero / boton eliminar)</p>
 	<p>&nbsp;</p>
 	<p>paginador  </p>-->
+
+	<script type="text/javascript">
+	  	$(document).ready(function() {
+
+	    	$('button#search').click(function(){
+	    		var text = document.getElementById('search_contact').value;
+	        	//console.log(text);
+	        	document.getElementById('search_contact').value = '';
+
+          		$.ajax({ type: "post",
+          			     data:{text:text},
+	                     url: "http://phonebook.com/search",
+			             success:  function (response) {
+			                        console.log(response);
+			             }
+
+	               });
+	          });
+
+	          //alert(response);*/
+
+	    });
+
+
+
+	</script>
+
 </body>
 </html>
